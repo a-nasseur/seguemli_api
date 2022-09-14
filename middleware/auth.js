4157d32b8f11b26e2044777ee580ff58a6731851
abdelhak <a.nasseur.dev@gmail.com>
2022-09-14 14:42:42 +0100

    updated error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
-    if(!token) return res.status(401).send('Access denied, nnauthorized request');
+    if(!token) return res.status(401).send('Access denied, unauthorized user');
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
